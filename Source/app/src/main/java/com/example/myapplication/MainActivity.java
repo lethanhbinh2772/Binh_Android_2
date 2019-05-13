aae1b63e4bf6e1736cffe755c513080facd718c3
lethanhbinh2772 <binhle2772@gmail.com>
2019-05-13 21:32:05 +0700

    Học Java buổi đầu.

--- Source/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Source/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //xin chào
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        //Tạo đối tượng User
+
+        User windsUser = new User();
+        /*windsUser.id = 1;
+        windsUser.setId(1);
+        windsUser.phoneNumber = "0123456789";
+        windsUser.profileUrl = "twitter.com/elonmusk";
+        windsUser.username = "BìnhLT";*/
+
+        windsUser.setId(1);
+        windsUser.setPhoneNumber("0123456789");
+        windsUser.setProfileUrl("twitter.com/elonmusk");
+        windsUser.setUsername("Lê Thanh Bình");
+
+        //Toast.makeText(this, windsUser.username, Toast.LENGTH_LONG).show();
+
+        //Sử dụng đối tượng  mà được khai báo và khởi tạo trong XML
+        /*Bước 1: gán ID cho đối tượng trong XML
+          Bước 2: tạo tên mới và gán lại cho đối tượng đấy
+         */
+        EditText edtPhoneNumber = findViewById(R.id.edt_phone_number);
+        edtPhoneNumber.setText("Elon Musk");
+
+        TextView tvUsername = findViewById(R.id.tv_username);
+        tvUsername.setText(windsUser.getUsername());
     }
 }
